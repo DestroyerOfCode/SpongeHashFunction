53ac2fa4e12fd8fb41fd6a0b35dca1a19390d925
marius babkovic <marius.babkovic9@gmail.com>
2023-11-02 22:36:39 +0100

    adding image to hash and changing main class

--- src/main/java/com/babkovic/Main.java
+++ src/main/java/com/babkovic/Main.java
@@ -2,6 +2,7 @@ package com.babkovic;
 
 import com.babkovic.api.SpongeHash;
 import com.babkovic.api.SpongePermutation;
+import com.babkovic.exception.SpongeException;
 import com.babkovic.keccak200sync.PermutationImpl;
 import com.babkovic.keccak200sync.SpongeHashKeccak200Impl;
 import java.io.FileInputStream;
@@ -14,11 +15,10 @@ public class Main {
     SpongePermutation permutation = new PermutationImpl();
     SpongeHash impl = new SpongeHashKeccak200Impl(permutation);
     try {
-      InputStream file =
-          new FileInputStream("/home/marius/Downloads/motherboard-internal-4k-k7.jpg");
+      InputStream file = new FileInputStream("src/main/resources/motherboard-internal-4k-k7.jpg");
       System.out.println("Hash: " + Arrays.toString(impl.hash(file, file.available())));
     } catch (IOException e) {
-      System.out.println("thrown exc");
+      throw new SpongeException("Something went wrong hashing", e);
     }
   }
 }
